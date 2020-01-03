57b5c29abed82f09de2b4c5da7040a6914d179da
baishafenfei <38197579+baishafenfei@users.noreply.github.com>
2020-01-03 09:41:05 +0800

    Update leetcode_20200103.c

--- c语言/leetcode_20200103.c
+++ c语言/leetcode_20200103.c
@@ -1,9 +1,19 @@
-int hammingWeight(uint32_t n) {
+int hammingWeight1(uint32_t n) {
     uint8_t numbers;
     while(n)
     {
         numbers += (n & 0x01);
         n>>=1;
+    }
+    return numbers;
+}
+
+int hammingWeight2(uint32_t n) {
+    uint8_t numbers;
+    while(n)
+    {
+       n = n & (n-1);
+       numbers++;
     }
     return numbers;
 }
